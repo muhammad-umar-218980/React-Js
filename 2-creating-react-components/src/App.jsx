e09c2bf0ccc9b076047b9d45736a8db96d782162
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-07-22 22:41:40 +0500

    Practicing Components and Notes added

---
+++ 2-creating-react-components/src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from './Welcome'
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+    </div>
+  )
+}
+
+export default App
---
+++ 2-creating-react-components/src/Welcome.jsx
@@ -0,0 +1,8 @@
+let Welcome = function(){
+  return <div>
+    <h1>Hello World</h1>
+    <p style={{ color: 'blue' }}>Welcome to React!</p>
+  </div>
+}
+
+export default Welcome
