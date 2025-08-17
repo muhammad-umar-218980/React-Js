277ac2a42424d825393fcef269017df06e7beb0d
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-08-18 02:04:14 +0500

    Footer Component Added

--- Projects/small-portfolio/src/App.jsx
+++ Projects/small-portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Skills />
         <Projects />
       </main>
+      <Footer />
     </>
   )
 }
---
+++ Projects/small-portfolio/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css'
+
+function Footer() {
+  
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="contact">
+          <p>Email: <a href="mailto:umar@example.com">umar@example.com</a></p>
+        </div>
+        <div className="copyright">
+          © 2025 Muhammad Umar. All Rights Reserved.
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
